fdec7932b2a23679c9825bbe5cbe38c7b59626c6
MarcusRowcliffe <marcus.rowcliffe@ioz.ac.uk>
2024-03-19 13:35:19 +0000

    Fixed mismatched braces in sbm class definition

--- R/sbm_class.r
+++ R/sbm_class.r
@@ -5,12 +5,13 @@
 #'  \itemize{
 #'   \item{estimate: }{A dataframe with fields \code{est}, \code{se}, \code{lcl},
 #'    and \code{ucl} (estimate, standard error, upper and lower confidence
-#'    limits), potentially with multiple rows if the model has covariates.
+#'    limits), potentially with multiple rows if the model has covariates.}
 #'   \item{model: }{A model fit of class \code{mle2}}.
-#'   \item{pdf: }{A text value recording the probability distribution of the model}.
+#'   \item{pdf: }{A text value recording the probability distribution of the model.}
 #'   \item{formula: }{A two-sided formula recording the response variable and
-#'    any covariates}.
-#'   \item{data: }{A dataframe holding the response variable and any covariates}.
+#'    any covariates.}
+#'   \item{data: }{A dataframe holding the response variable and any covariates.}
+#'  }
 #'
 #' @export
 setClass("sbm", representation("list"))
