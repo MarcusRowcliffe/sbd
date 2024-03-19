f894b1cf831810f0081839b311ecefdb7a74f8dc
MarcusRowcliffe <marcus.rowcliffe@ioz.ac.uk>
2024-03-19 12:46:18 +0000

    plot.sbm help added

--- R/plot.sbm.r
+++ R/plot.sbm.r
@@ -1,11 +1,30 @@
-
-#Plot a size biased model data and fitted distributions
-#INPUT
-# obj: a size biased model fitted with sbm
-# log: whether to plot the distribution log scale
-# lpar: plotting paramaters defining fitted line characteristics
-# ppar: plot parameters passed to plot
-# hpar: parameters passed histogram (not plotting parameters - pass these to ppar)
+#' Plot size biased distribution
+#'
+#' Plots the frequency distribution of data from a size biased model, with
+#' fitted parametric distribution if the model is parametric and used no
+#' covariates.
+#'
+#' @param mod A size biased model fit of class \code{\link{sbm}}.
+#' @param log A logical specifying whether to plot the log transformed or
+#'  untransformed data distribution.
+#' @param add A logical specifying whether to create a new plot, or add a
+#'  parametric distribution curve to an existing plot.
+#' @param ppar A list of plotting parameters passed to plot; accepts
+#'  arguments controlling both overall plot appearance and graphical elements
+#'  of the histogram.
+#' @param hpar A list of parameters used when creating the frequency data;
+#'  primarily used to determine breaks.
+#' @param lpar A list of plotting parameters controlling appearance of the
+#'  density curve line (if present).
+#' @return None.
+#' @examples
+#'   data(sbdData)
+#'   lmod <- sbm(speed~1, sbdData, pdf="lnorm")
+#'   wmod <- sbm(speed~1, sbdData, pdf="weibull")
+#'   plot(lmod, hpar=list(breaks = 40))
+#'   plot(wmod, add=TRUE, lpar=list(col="blue"))
+#' @export
+#'
 plot.sbm <- function(mod, log=TRUE, add=FALSE,
                      ppar=list(col=NULL), hpar=list(), lpar=list(col="red")){
   allvars <- all.vars(mod$formula)
